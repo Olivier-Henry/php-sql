91e9ae42ce6071e02b605a4623af133509976559
formation <formation@172.26.10.85>
2016-03-22 11:53:10 +0100

    Ajout d'une première requete et affichage d'un résultat

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 $dsn = "mysql:host=127.0.0.1;dbname=formation_sql";
 $connexion = new PDO($dsn, 'root', '');
 
+//var_dump($connexion);
+
+$rs = $connexion->query("SELECT * FROM personnes");
+
+//var_dump($rs);
+
+foreach ($rs as $row){
+    //var_dump($row);
+    echo $row['nom']."<br>";
+}

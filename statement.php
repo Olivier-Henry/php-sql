df2c7adf353ad530c97766df6b0db9c49326ac6a
formation <formation@172.26.10.85>
2016-03-22 12:55:15 +0100

    Test requetes préparées et  rollback

--- statement.php
+++ statement.php
@@ -1,13 +1,57 @@
 <?php
 
-
 //exemple de requete pouvant prendre des injection sql
-$sql = "SELECT * FROM personnes WHERE id=". $_GET['id'];
+//$sql = "SELECT * FROM personnes WHERE id=". $_GET['id'];
+//
+//echo $sql;
+//
+//
+////Il vaut mieux utiliser pour s'en prémunir
+//$sql = "SELECT * FROM personnes WHERE id='". $_GET['id']."'";
+//
+//echo $sql;
 
-echo $sql;
+
+$dsn = "mysql:host=127.0.0.1;dbname=formation_sql";
+$connexion = new PDO($dsn, 'root', '', array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ));
 
 
-//Il vaut mieux utiliser pour s'en prémunir
-$sql = "SELECT * FROM personnes WHERE id='". $_GET['id']."'";
+try {
 
-echo $sql;
+    //pour un rollback
+    $connexion->beginTransaction();
+
+    // Les prepared statements (requêtes préparées)
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM personnes WHERE personne_id=?";
+    //$sql = "SELECT * FROM personnes WHERE personne_id=:id"; <--- pour passer un tableau associatif
+
+    $statement = $connexion->prepare($sql);
+    $statement->execute([$id]);
+    //$statement->execute(['id' => $id]); <- dans le cas tab associatif
+    $rs = $statement->fetch(PDO::FETCH_OBJ);
+    var_dump($rs);
+
+    //autre exemple
+    $sql = "INSERT INTO personnes (nom, prenom, date_naissance) VALUES(:nom, :prenom, :date)";
+    $statement = $connexion->prepare($sql);
+
+    $data = array(
+        array('nom' => 'Lovelace', 'prenom' => 'Ada', 'date' => null),
+        array('nom' => 'Lovelace', 'prenom' => 'Georges', 'date' => null)
+    );
+
+    foreach ($data as $personne) {
+        $statement->execute($personne);
+    }
+
+    //Pour le rollback
+    $connexion->commit();
+} catch (PDOException $ex) {
+    echo $ex->getMessage();
+
+    //rollback
+    $connexion->rollBack();
+}
